8e7219b34d903fafd1e3754342b0083ba787c6e1
Uri Gruda <uri2920@gmail.com>
2022-07-04 15:22:48 +0300

    Connected to atlas DB

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,6 @@
+const username = encodeURIComponent('Eugee')
+const password = encodeURIComponent('qBap7XTvogtvRkg4')
+
 module.exports = {
-  'dbURL': 'mongodb+srv://eran:7O1ETPJi6o6lPXz4@cluster0.ek0xl.mongodb.net/?retryWrites=true&w=majority',
+  dbURL: `mongodb+srv://${username}:${password}@cluster0.mm0ij.mongodb.net/?retryWrites=true&w=majority`,
 }
