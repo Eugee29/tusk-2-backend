1def31927f3215dec188cf86a30a60643a2eecd6
Uri Gruda <uri2920@gmail.com>
2022-07-04 15:15:32 +0300

    made dashboard responsive

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -19,8 +19,6 @@ async function login(req, res) {
 async function signup(req, res) {
   try {
     const { username, password, fullname } = req.body
-    // Never log passwords
-    // logger.debug(fullname + ', ' + username + ', ' + password)
     const account = await authService.signup(username, password, fullname)
     logger.debug(`auth.route - new account created: ` + JSON.stringify(account))
     const user = await authService.login(username, password)
@@ -47,5 +45,5 @@ async function logout(req, res) {
 module.exports = {
   login,
   signup,
-  logout
+  logout,
 }
